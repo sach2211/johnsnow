e61c1f6d05cc81870bb278fff41222bfe2fc3ecc
Sachin Chopra <sachin.chopra@myntra.com>
2016-04-25 15:23:44 +0530

    Experimenting with webhooks

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.status(200).send('Hello World!');
 });
 
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === 'whitewalkers') {
-    res.send(req.query['hub.challenge']);
+    res.status(200).send(req.query['hub.challenge']);
   }
-  res.send('Error, wrong validation token');
+  else 
+    res.status(200).send('Error, wrong validation token');
 })
 
 app.listen(5000, function () {
